17f01402b0764d70f39dcd47ce7be7cb1fbe8e33
kumud soni <soni.kumud13@gmail.com>
2020-06-25 11:00:57 +0100

    committing code for base project

---
+++ springboot_rest_curdapi/src/main/java/com/zensar/springboot/SpringbootRestCurdapiApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestCurdapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestCurdapiApplication.class, args);
+	}
+
+}
